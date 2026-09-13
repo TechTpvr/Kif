7bf1a89c5e1fdcfcea5c2a879242ba5c448775a5
TechTpvr <farrokhzadpvr@gmail.com>
2026-09-13 18:19:40 +0330

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='kifnet-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./assets/logo.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{const c=x.clone();caches.open(CACHE).then(k=>k.put(e.request,c));return x}).catch(()=>caches.match('./index.html')))));
