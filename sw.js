db44afe798e4a62c7b69d148240f0a16e4439a62
TechTpvr <farrokhzadpvr@gmail.com>
2026-09-13 22:25:39 +0330

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,1 @@
-const CACHE="kifnet-v4-restored";
-const ASSETS=["./","./index.html","./manifest.webmanifest","./logo.png","./logo-bbx.png","./logo-open.jpg","./logo-dark.jpg"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener("fetch",e=>{
-  if(e.request.method!=="GET") return;
-  const u=new URL(e.request.url);
-  if(u.origin!==location.origin) return;
-  e.respondWith(fetch(e.request).then(r=>{
-    const copy=r.clone();
-    caches.open(CACHE).then(c=>c.put(e.request,copy));
-    return r;
-  }).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html"))));
-});
+const CACHE='kifnet-v3';const ASSETS=['./','./index.html','./manifest.webmanifest','./logo.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(r=>{if(new URL(e.request.url).origin===location.origin){const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy))}return r}).catch(()=>cached)))})
